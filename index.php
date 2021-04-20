55c7ff2b8b385ae031ee8c2d581b6b1eb5d8c3a6
Eashune Abenojar <EashuneAbenojar@hotmail.com>
2021-04-20 13:02:30 -0400

    Added an index & views directory containing the home page

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Instantiate the base class
+$f3 = Base::instance();
+// Base f3 = new Base();
+
+//Define a default route
+$f3->route('GET /', function (){
+    // Display the home page
+    $view = new Template();
+    echo $view -> render('views/home.html');
+});
+
+//Run fat free
+$f3->run();
